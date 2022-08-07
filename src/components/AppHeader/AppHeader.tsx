af34d689109b125246fe23124b72e2fdfc27569f
klexq <daniilevgaliev@yandex.ru>
2022-08-07 16:57:40 +0400

    first

---
+++ src/components/AppHeader/AppHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import headerStyle from './appHeader.module.css';
+
+const AppHeader = () => {
+  return <div className={headerStyle.header}></div>;
+};
+
+export default AppHeader;
